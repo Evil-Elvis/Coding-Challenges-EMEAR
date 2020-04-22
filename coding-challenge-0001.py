d6a8df9a1ea198f78b71e29e5aa69af9ba2f6c4e
Evil-Elvis <64075103+Evil-Elvis@users.noreply.github.com>
2020-04-22 07:21:47 +0200

    Update coding-challenge-0001.py

--- coding-challenge-0001.py
+++ coding-challenge-0001.py
@@ -1,3 +1,16 @@
+"""
+Open your PyCharm Or IDLE any Editor of your preference
+Declare three variables named random_number and sum_of_digits and product_of_digits.
+Initialize these variables with 0 since they will contain integers [numbers].
+Then instruct the Python Interpreter to randomly choose a value of two digit between 10 & 99. 
+Save that random value into the appropriate variable (random_number).
+Compute the sum of digits inside this random number and save it into sum_of_digits.
+Compute the product of all digits inside this number and save into product_of_digits.
+Display the two result to the user and prompt the user to imagine/guess and type this random number.
+Compare this number with the random value and inform the user either it is higher or lower or equal.
+In event the user number matches with the random number then congrats the user :)
+"""
+# defining variable
 random_number = (0)
 sum_of_digits = (0)
 product_of_digits = (0)
